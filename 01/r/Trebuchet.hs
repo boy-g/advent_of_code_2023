9b8338355716bb015b4f9bf98260a882d89d712b
byllgrim <myemail>
2023-12-02 19:51:57 +0100

    solve first example

--- 01/r/Trebuchet.hs
+++ 01/r/Trebuchet.hs
@@ -1,5 +1,24 @@
 module Main (main) where
 
+import Data.Char (isDigit)
+
 main :: IO ()
 main = do
-  print "hey ho"
+  puzzleInput <- getContents
+  print $ processPuzzleInput puzzleInput
+
+processPuzzleInput puzzleInput =
+  sum processedPuzzleLines
+  where
+    puzzleLines = lines puzzleInput
+    processedPuzzleLines = map processPuzzleLine puzzleLines
+
+processPuzzleLine puzzleLine =
+  calibrationValue
+  where
+    digitsLine = filter isDigit puzzleLine
+    digitsPair = extractFirstLastPair digitsLine
+    calibrationValue = read digitsPair :: Integer
+
+extractFirstLastPair digitsLine =
+  [head digitsLine, last digitsLine]
