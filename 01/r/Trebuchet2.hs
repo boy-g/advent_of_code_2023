bd9cb4afc4584c495f80c35ad4d6e8f3e9637f7e
byllgrim <myemail>
2023-12-03 20:38:13 +0100

    solve example of d01p02

--- 01/r/Trebuchet2.hs
+++ 01/r/Trebuchet2.hs
@@ -9,20 +9,18 @@ main = do
   print $ processPuzzleInput puzzleInput
 
 processPuzzleInput puzzleInput =
-  --sum processedPuzzleLines
-  processedPuzzleLines
+  sum processedPuzzleLines
   where
     puzzleLines = lines puzzleInput
     processedPuzzleLines = map processPuzzleLine puzzleLines
 
 processPuzzleLine puzzleLine =
-  --calibrationValue
-  digifiedPuzzleLine
+  calibrationValue
   where
-    digitsLine = filter isDigit puzzleLine
-    digitsPair = extractFirstLastPair digitsLine
-    calibrationValue = read digitsPair :: Integer
     digifiedPuzzleLine = digifyPuzzleLine puzzleLine
+    digitsLine         = filter isDigit digifiedPuzzleLine
+    digitsPair         = extractFirstLastPair digitsLine
+    calibrationValue   = read digitsPair :: Integer
 
 extractFirstLastPair digitsLine =
   [head digitsLine, last digitsLine]
@@ -36,10 +34,16 @@ consumeDigits :: ([Char], [Char]) -> ([Char], [Char])
 consumeDigits (outputLine, []) =
   (outputLine, [])  -- TODO
 consumeDigits (outputLine, inputLine)
-  | isDigit nextChar             = (outputLine ++ [nextChar], inputLine)  -- TODO
-  | isPrefixOf "one" inputLine   = (outputLine ++ "1", inputLine)  -- TODO
-  | isPrefixOf "two" inputLine   = consumeDigits (outputLine ++ "2", drop 3 inputLine)  -- TODO
-  | isPrefixOf "three" inputLine = (outputLine ++ "3", inputLine)  -- TODO
-  | otherwise                    = (outputLine, inputLine)  -- TODO
+  | isDigit nextChar             = consumeDigits (outputLine ++ [nextChar], tail inputLine)
+  | isPrefixOf "one"   inputLine = consumeDigits (outputLine ++ "1", drop 3 inputLine)
+  | isPrefixOf "two"   inputLine = consumeDigits (outputLine ++ "2", drop 3 inputLine)
+  | isPrefixOf "three" inputLine = consumeDigits (outputLine ++ "3", drop 5 inputLine)
+  | isPrefixOf "four"  inputLine = consumeDigits (outputLine ++ "4", drop 4 inputLine)
+  | isPrefixOf "five"  inputLine = consumeDigits (outputLine ++ "5", drop 4 inputLine)
+  | isPrefixOf "six"   inputLine = consumeDigits (outputLine ++ "6", drop 3 inputLine)
+  | isPrefixOf "seven" inputLine = consumeDigits (outputLine ++ "7", drop 5 inputLine)
+  | isPrefixOf "eight" inputLine = consumeDigits (outputLine ++ "8", drop 5 inputLine)
+  | isPrefixOf "nine"  inputLine = consumeDigits (outputLine ++ "9", drop 4 inputLine)
+  | otherwise                    = consumeDigits (outputLine, tail inputLine)
   where
     nextChar = head inputLine
