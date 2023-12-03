38c472d5cb0c4b52abe7d13f3427bd1963ea8c0a
byllgrim <myemail>
2023-12-03 20:27:21 +0100

    gaining traction on d01p02

--- 01/r/Trebuchet2.hs
+++ 01/r/Trebuchet2.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Data.Char (isDigit)
+import Data.List (isPrefixOf)
 
 main :: IO ()
 main = do
@@ -16,11 +17,29 @@ processPuzzleInput puzzleInput =
 
 processPuzzleLine puzzleLine =
   --calibrationValue
-  puzzleLine
+  digifiedPuzzleLine
   where
     digitsLine = filter isDigit puzzleLine
     digitsPair = extractFirstLastPair digitsLine
     calibrationValue = read digitsPair :: Integer
+    digifiedPuzzleLine = digifyPuzzleLine puzzleLine
 
 extractFirstLastPair digitsLine =
   [head digitsLine, last digitsLine]
+
+digifyPuzzleLine puzzleLine =
+  digifiedPuzzleLine
+  where
+    (digifiedPuzzleLine, _) = consumeDigits ([], puzzleLine)
+
+consumeDigits :: ([Char], [Char]) -> ([Char], [Char])
+consumeDigits (outputLine, []) =
+  (outputLine, [])  -- TODO
+consumeDigits (outputLine, inputLine)
+  | isDigit nextChar             = (outputLine ++ [nextChar], inputLine)  -- TODO
+  | isPrefixOf "one" inputLine   = (outputLine ++ "1", inputLine)  -- TODO
+  | isPrefixOf "two" inputLine   = consumeDigits (outputLine ++ "2", drop 3 inputLine)  -- TODO
+  | isPrefixOf "three" inputLine = (outputLine ++ "3", inputLine)  -- TODO
+  | otherwise                    = (outputLine, inputLine)  -- TODO
+  where
+    nextChar = head inputLine
