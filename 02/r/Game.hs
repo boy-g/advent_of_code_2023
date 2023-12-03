88a9d3cc24e13d2ba5cd0587196bb4c81c6eb866
byllgrim <myemail>
2023-12-03 21:25:24 +0100

    define datatypes for d02

--- 02/r/Game.hs
+++ 02/r/Game.hs
@@ -1,6 +1,23 @@
 module Main (main) where
 
+data Hand = Hand {
+  red   :: Integer,
+  green :: Integer,
+  blue  :: Integer
+} deriving (Show)
+
+data Game = Game {
+  id    :: Integer,
+  hands :: [Hand]
+} deriving (Show)
+
+readStdin :: IO String
+readStdin = getContents
+
 main :: IO ()
 main = do
-  --puzzleInput <- readStdin  (aka getContents)
-  print "hey ho"
+  puzzleInput <- readStdin
+  print $ parseInput puzzleInput
+
+parseInput :: String -> [Game]
+parseInput _ = [Game 0 []]
