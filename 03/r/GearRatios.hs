584891ae3ca633fb0e96e781a31471ae47ce1b07
byllgrim <myemail>
2023-12-22 22:33:40 +0100

    d3: locate direct adjacents

--- 03/r/GearRatios.hs
+++ 03/r/GearRatios.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.Char (isNumber)
 import Debug.Trace (trace)
 
 readStdin :: IO String
@@ -8,31 +9,82 @@ readStdin = getContents
 --TODO data Map = Map [[Char]]
 
 data Coord = Coord {
-  x :: Integer,
-  y :: Integer
+  x :: Int,
+  y :: Int
 } deriving (Show)
 
 main :: IO ()
 main = do
   puzzleInput <- readStdin
-  print $ locateAdjacents $ parsePuzzleInput puzzleInput
+  print $ locateAdjacents $ keepOnlyNumberCoordinates $ listifyAllCoordinates $ parsePuzzleInput puzzleInput
+
+keepOnlyNumberCoordinates :: ([[Char]], [Coord]) -> ([[Char]], [Coord])
+keepOnlyNumberCoordinates (themap, coordsOld) =
+  (themap, coordsNumbers)
+  where
+    coordsNumbers = filter (isNumCoord themap) coordsOld
+
+isNumCoord :: [[Char]] -> Coord -> Bool
+isNumCoord themap (Coord {x, y}) =
+  isNumber mapDataAtCoord
+  where
+    mapDataAtCoord = (themap !! y) !! x
+
+listifyAllCoordinates :: [[Char]] -> ([[Char]], [Coord])
+listifyAllCoordinates themap =
+  (themap, coordsAll)
+  where
+    height = length themap
+    width  = length $ head themap
+    ys = [0..(height-1)]
+    xs = [0..(width-1)]
+    coordsAll         =
+      [Coord {x=x, y=y} | x<-xs, y<-ys]
 
 parsePuzzleInput :: String -> [[Char]]
 parsePuzzleInput puzzleInput =
   lines puzzleInput
 
-locateAdjacents :: [[Char]] -> ([[Char]], [Coord])
-locateAdjacents map =
-  (map, coordsHasAdjacent)
+locateAdjacents :: ([[Char]], [Coord]) -> ([[Char]], [Coord])
+locateAdjacents (themap, coordsAll) =
+  (themap, coordsHasAdjacent)
   where
-    height = toInteger $ length map
-    width  = toInteger $ length $ head map
-    ys = [0..(height-1)] :: [Integer]
-    xs = [0..(width-1)]
-    coordsAll         =
-      [Coord {x=x, y=y} | x<-xs, y<-ys]
-    coordsHasAdjacent = filter (hasAdjacentOnMap map) coordsAll
+    coordsHasAdjacent = filter (hasAdjacentOnMap themap) coordsAll
 
 hasAdjacentOnMap :: [[Char]] -> Coord -> Bool
-hasAdjacentOnMap map coord =
-  True  --TODO
+hasAdjacentOnMap themap coord =
+  0 /= length mapDataSymbols
+  where
+    coordsNeighborsAll    = genNeighborCoords coord
+    coordsNeighborsActual = filter (isOnMap themap) coordsNeighborsAll
+    mapDataNeighbors = map (getMapDataAt themap) coordsNeighborsActual
+    mapDataSymbols   = filter isSymbol mapDataNeighbors
+
+genNeighborCoords :: Coord -> [Coord]
+genNeighborCoords (Coord {x, y}) =
+  [
+    Coord {x=x+1, y=y+0},
+    Coord {x=x+1, y=y+1},
+    Coord {x=x+0, y=y+1},
+    Coord {x=x-1, y=y+1},
+    Coord {x=x-1, y=y+0},
+    Coord {x=x-1, y=y-1},
+    Coord {x=x-0, y=y-1},
+    Coord {x=x+1, y=y-1}
+  ]
+
+isOnMap :: [[Char]] -> Coord -> Bool
+isOnMap themap (Coord {x, y}) =
+  0 < x  &&
+  0 < y  &&
+  x < (length themap)  &&
+  y < (length $ head themap)
+
+getMapDataAt :: [[Char]] -> Coord -> Char
+getMapDataAt themap (Coord {x, y}) =
+  (themap !! y) !! x
+
+isSymbol :: Char -> Bool
+isSymbol c =
+  (not $ isNumber c)  &&
+  (not $ c == '.')
