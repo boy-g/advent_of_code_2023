a4402ccf68e8969c0123b104be8a5aa5f6a2dcef
byllgrim <myemail>
2023-12-23 23:40:02 +0100

    d5: parse seeds

--- 05/r/Fertilizer.hs
+++ 05/r/Fertilizer.hs
@@ -1,6 +1,46 @@
 module Main (main) where
 
+
+import Debug.Trace (trace)
+
+
+data Almanac =
+  Almanac {
+    almanacSeeds :: [Int],
+    almanacMaps  :: [Map]
+  }
+  deriving (Show)
+
+data Map =
+  Map [Conversion]
+  deriving (Show)
+
+data Conversion =
+  Conversion {
+    conversionDestination :: Int,
+    conversionSource      :: Int,
+    conversionLength      :: Int
+  }
+  deriving (Show)
+
+
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print puzzleInput
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> Almanac
+parsePuzzleInput puzzleInput =
+  almanac
+  where
+    almanac = Almanac seeds maps
+    seeds   = parseSeeds puzzleInput
+    maps    = []  --TODO
+
+parseSeeds :: String -> [Int]
+parseSeeds puzzleInput =
+  seeds
+  where
+    firstLine   = head $ lines puzzleInput
+    seedsString = tail $ words firstLine
+    seeds       = map read seedsString
