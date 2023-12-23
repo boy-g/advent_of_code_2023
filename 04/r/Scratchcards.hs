c4f89dd5ddea49d71ab49d7cc75eb4f23109e5fb
byllgrim <myemail>
2023-12-23 10:10:00 +0100

    start d4

--- 04/r/Scratchcards.hs
+++ 04/r/Scratchcards.hs
@@ -1,5 +1,41 @@
 module Main (main) where
 
+import Data.List.Split (splitOn)
+import Debug.Trace (trace)
+
+data Card = Card {
+  cardWins  :: [Int],
+  cardHaves :: [Int]
+} deriving (Show)
+
+readStdin :: IO String
+readStdin =
+  getContents
+
 main :: IO ()
 main = do
-  print "hey ho TODO"
+  puzzleInput <- readStdin
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> [Card]
+parsePuzzleInput puzzleInput =
+  [parseLineCard $ puzzleLines !! 0]  --TODO all
+  where
+    puzzleLines = lines puzzleInput
+
+parseLineCard :: String -> Card
+parseLineCard line =
+  trace (show (wins, haves)) Card {cardWins=wins, cardHaves=haves}
+  where
+    lineWoPrefix   = last $ splitOn ":" line
+    linesWinsHaves = splitOn "|" lineWoPrefix
+    winsStringsWSpaces  = splitOn " " $ head linesWinsHaves
+    havesStringsWSpaces = splitOn " " $ last linesWinsHaves
+    winsStrings  = filter isNonEmptyString winsStringsWSpaces
+    havesStrings = filter isNonEmptyString havesStringsWSpaces
+    wins  = map read winsStrings :: [Int]
+    haves = map read havesStrings :: [Int]
+
+isNonEmptyString :: String -> Bool
+isNonEmptyString s =
+  0 < length s
