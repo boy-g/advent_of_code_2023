29f65dcc9c1be52f47a0b9f4fdea5c1eda01bc6c
byllgrim <myemail>
2023-12-26 00:35:12 +0100

    d6: parse puzzle input

--- 06/r/Wait.hs
+++ 06/r/Wait.hs
@@ -1,6 +1,38 @@
 module Main (main) where
 
+
+data Paper =
+  Paper [Record]
+  deriving (Show)
+
+data Record =
+  Record {
+    time     :: Integer,
+    distance :: Integer
+  }
+  deriving (Show)
+
+
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print puzzleInput
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> Paper
+parsePuzzleInput puzzleInput =
+  Paper records
+  where
+    times     = parseLine $ head $ lines puzzleInput
+    distances = parseLine $ last $ lines puzzleInput
+    records   = map recordifyTuple $ zip times distances
+
+recordifyTuple :: (Integer, Integer) -> Record
+recordifyTuple (time, distance) =
+  Record { time=time, distance=distance }
+
+parseLine :: String -> [Integer]
+parseLine line =
+  milliseconds
+  where
+    millisecondsString = tail $ words line
+    milliseconds       = map read millisecondsString
