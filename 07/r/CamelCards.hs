6465b13f33dca0ddd6e13b3b16e481a9b053b44c
byllgrim <myemail>
2023-12-26 01:42:58 +0100

    d7: parse puzzle input

--- 07/r/CamelCards.hs
+++ 07/r/CamelCards.hs
@@ -1,6 +1,43 @@
 module Main (main) where
 
+
+data Game =
+  Game {
+    gameHandBids :: [HandBid]
+  }
+  deriving (Show)
+
+data HandBid =
+  HandBid (Hand, Bid)
+  deriving (Show)
+
+data Hand =
+  Hand {
+    handLabels :: [Char]
+  }
+  deriving (Show)
+
+data Bid =
+  Bid Integer
+  deriving (Show)
+
+
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print puzzleInput
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> Game
+parsePuzzleInput puzzleInput =
+  Game {gameHandBids=handBids}
+  where
+    handBids    = map parseLine puzzleLines
+    puzzleLines = lines puzzleInput
+
+parseLine :: String -> HandBid
+parseLine line =
+  handbid
+  where
+    handbid = HandBid (Hand hand, Bid bid)
+    hand    = head $ words line
+    bid     = read $ last $ words line
