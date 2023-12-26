935d4d6260b1049ce96b8f7f74387ece26764b1f
byllgrim <myemail>
2023-12-26 18:40:05 +0100

    d7: start ordering

--- 07/r/CamelCards.hs
+++ 07/r/CamelCards.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
 
-import Data.List   (sort)
+import Data.List   (sort, isInfixOf)
+import Data.Set    (fromList)
 import Debug.Trace (trace)
 
 
@@ -17,23 +18,34 @@ data Line =
     lineBid  :: Bid,
     lineRank :: Maybe Rank
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
---TODO instance Line Ord
+instance Ord Line where
+  compare = lineCompare
 
 data Rank =
   Rank Integer
-  deriving (Show)
+  deriving (Show, Eq)
 
 data Hand =
   Hand {
     handLabels :: [Char]
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data Bid =
   Bid Integer
-  deriving (Show)
+  deriving (Show, Eq)
+
+data HandType =
+  HighCard     |
+  OnePair      |
+  TwoPair      |
+  ThreeOfAKind |
+  FullHouse    |
+  FourOfAKind  |
+  FiveOfAKind
+  deriving (Eq, Ord)
 
 
 main :: IO ()
@@ -45,8 +57,8 @@ solvePuzzle :: Game -> Integer
 solvePuzzle gameUnordered =
   trace (show gameOrdered) 0  -- TODO
   where
-    Game { gameLines=lines } = gameUnordered
-    linesOrdered = lines  --TODO sort lines
+    Game { gameLines=linesUnordered } = gameUnordered
+    linesOrdered = sort linesUnordered
     gameOrdered = Game { gameLines=linesOrdered }
 
 parsePuzzleInput :: String -> Game
@@ -63,3 +75,60 @@ parseLine inputLine =
     gameLine = Line { lineHand=hand, lineBid=bid, lineRank=Nothing }
     hand     = Hand $ head $ words inputLine
     bid      = Bid $ read $ last $ words inputLine
+
+lineCompare :: Line -> Line -> Ordering
+lineCompare lineX lineY
+  | handTypeX == handTypeY =
+    EQ  -- TODO EQ not possible, resolve
+  | otherwise =
+    compare handTypeX handTypeY
+  where
+    handTypeX = getHandType $ lineHand lineX
+    handTypeY = getHandType $ lineHand lineY
+
+getHandType :: Hand -> HandType
+getHandType hand
+  | lengthSet == 5 =
+    HighCard
+  | lengthSet == 4 =
+    OnePair
+  | lengthSet == 3 && not (hasThrees hand) =
+    TwoPair
+  | lengthSet == 3 =
+    ThreeOfAKind
+  | lengthSet == 2 && not (hasFours hand) =
+    FullHouse
+  | lengthSet == 2 =
+    FourOfAKind
+  | lengthSet == 1 =
+    FiveOfAKind
+  | otherwise =
+    error $ "getHandType: " ++ show hand
+  where
+    lengthSet  = length $ fromList $ handLabels hand
+
+hasThrees :: Hand -> Bool
+hasThrees hand =
+  has
+  where
+    has             = hasFirst || hasSecond || hasThird
+    labelsOrdered   = sort labelsUnordered
+    labelsUnordered = handLabels hand
+    threeFirst      = take 3 $ repeat $ labelsOrdered !! 0
+    threeSecond     = take 3 $ repeat $ labelsOrdered !! 1
+    threeThird      = take 3 $ repeat $ labelsOrdered !! 2
+    hasFirst        = isInfixOf threeFirst labelsOrdered
+    hasSecond       = isInfixOf threeSecond labelsOrdered
+    hasThird        = isInfixOf threeThird labelsOrdered
+
+hasFours :: Hand -> Bool
+hasFours hand =
+  has
+  where
+    has             = hasFirst || hasSecond
+    hasFirst        = isInfixOf fourFirst labelsOrdered
+    hasSecond       = isInfixOf fourSecond labelsOrdered
+    fourFirst       = take 4 $ repeat $ labelsOrdered !! 0
+    fourSecond      = take 4 $ repeat $ labelsOrdered !! 1
+    labelsOrdered   = sort labelsUnordered
+    labelsUnordered = handLabels hand
