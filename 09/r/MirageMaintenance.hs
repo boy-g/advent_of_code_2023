2b51c1ee4affeff2c9448722d891901ab119c437
byllgrim <myemail>
2023-12-27 15:10:51 +0100

    d9: parse input

--- 09/r/MirageMaintenance.hs
+++ 09/r/MirageMaintenance.hs
@@ -1,6 +1,32 @@
 module Main (main) where
 
+
+data Report =
+  Report {
+    reportHistories:: [History]
+  }
+  deriving (Show)
+
+data History =
+  History {
+    historyValues :: [Integer]
+  }
+  deriving (Show)
+
+
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print puzzleInput
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> Report
+parsePuzzleInput puzzleInput = report
+  where
+  report    = Report histories
+  histories = map parseLine $ lines puzzleInput
+
+parseLine :: String -> History
+parseLine line = history
+  where
+  history = History values
+  values  = map read $ words line
