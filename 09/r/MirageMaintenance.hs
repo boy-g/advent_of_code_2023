7c0dbca2e5f61000d1701a0dc80cae759a90be3e
byllgrim <myemail>
2023-12-28 01:04:08 +0100

    d9: generate diff-sequences

--- 09/r/MirageMaintenance.hs
+++ 09/r/MirageMaintenance.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
 
+import Debug.Trace (trace)
+
+
 data Report =
   Report {
     reportHistories:: [History]
@@ -13,11 +16,17 @@ data History =
   }
   deriving (Show)
 
+data Sequences =
+  Sequences {
+    sequencesValss :: [[Integer]]
+  }
+  deriving (Show)
+
 
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print $ parsePuzzleInput puzzleInput
+  print $ solvePuzzle $ parsePuzzleInput puzzleInput
 
 parsePuzzleInput :: String -> Report
 parsePuzzleInput puzzleInput = report
@@ -30,3 +39,38 @@ parseLine line = history
   where
   history = History values
   values  = map read $ words line
+
+solvePuzzle :: Report -> Integer
+solvePuzzle report = trace (show extrapolated0) 0
+  where
+  seq0          = historyValues $ head $ reportHistories report
+  diffSeqs0     = generateDiffSequences [seq0]
+  extrapolated0 = extrapolate diffSeqs0
+  -- TODO all lines
+
+generateDiffSequences :: [[Integer]] -> [[Integer]]
+generateDiffSequences seqsOld
+  | isDiffSequencesDone seqsOld =
+    seqsOld
+  | otherwise =
+    generateDiffSequences $ seqsOld ++ [seqNew]
+  where
+  seqNew = calcDiffs $ last seqsOld
+
+isDiffSequencesDone :: [[Integer]] -> Bool
+isDiffSequencesDone seqs = isDone
+  where
+  isDone  = seqLast == take (length seqLast) seq0
+  seqLast = last seqs
+  seq0    = [0, 0..]
+
+calcDiffs :: [Integer] -> [Integer]
+calcDiffs valsIn = diffs
+  where
+  steps = zip valsIn $ tail valsIn
+  diffs = map (\(a, b) -> b - a) steps
+
+extrapolate :: [[Integer]] -> [[Integer]]
+extrapolate seqsOld = seqsNew
+  where
+  seqsNew = seqsOld  -- TODO
