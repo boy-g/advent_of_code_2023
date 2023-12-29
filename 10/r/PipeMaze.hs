e557bf792c990ea497d346f3c1871257b5be9720
byllgrim <myemail>
2023-12-29 10:02:21 +0100

    d10: parse input

--- 10/r/PipeMaze.hs
+++ 10/r/PipeMaze.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
+
+data SketchOfPipes =
+  SketchOfPipes [[Char]]
+  deriving (Show)
+
+
 main :: IO ()
 main = do
   puzzleInput <- getContents
-  print puzzleInput
+  print $ parsePuzzleInput puzzleInput
+
+parsePuzzleInput :: String -> SketchOfPipes
+parsePuzzleInput puzzleInput = sketch
+  where
+  sketch       = SketchOfPipes linesOfPipes
+  linesOfPipes = lines puzzleInput
